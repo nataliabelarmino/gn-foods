46ed9241fe6d5270f9669849de6aa3ec4274a954
Gabrieltrembala12 <gabriel.silva.12@academico.ifpb.edu.br>
2024-11-26 12:04:03 -0300

    BD

---
+++ src/app/aluno/page.js
@@ -0,0 +1,11 @@
+import db from "@/lib/db";
+
+export default async () => {
+
+        const categorias = await db.query("select * from categoria");
+
+    return (<>
+        <h1>Lista de categorias</h1>
+        <div>{categorias.rows.map(c => <div>{c.nome}</div>)}</div>
+    </>);
+}
